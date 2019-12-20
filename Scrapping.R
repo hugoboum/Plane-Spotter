72463e379be018f9841ac406f305444418c38f36
Florentin Coeurdoux <florentincoeurdoux@pc54.home>
2019-12-20 20:31:28 +0100

    First commit

---
+++ Scrapping.R
@@ -0,0 +1,50 @@
+library(rvest)
+library(dplyr)
+library(NLP)
+
+
+planespotter <- read_html("https://www.planespotters.net/photos/latest?page=1")
+
+img <- planespotter %>%
+  html_node(xpath = '//*/img')
+
+imgalt <- html_attr(img, 'alt')
+imgsrc <- html_attr(img, 'src')
+imgsrc_split <- strsplit(imgsrc, "_")
+img_number <- as.integer(imgsrc_split[[1]][3])
+for(j in 1:47) {
+  img_numbera <- img_number - j
+  node_id <- paste0('#', img_numbera, ' .photo__intrinsic_item')
+  
+  imgsrc <- planespotter %>%
+    html_node(node_id) %>%
+    html_attr('src')
+  imgsrc
+  
+  download.file(imgsrc, destfile = basename(imgsrc))
+}
+
+for (i in 1:125){
+  planespotter_url <- paste("https://www.planespotters.net/photos/latest?page=",i, sep = "")
+  planespotter <- read_html(planespotter_url)
+  img <- planespotter %>%
+    html_node(xpath = '//*/img')
+  
+  imgalt <- html_attr(img, 'alt')
+  imgsrc <- html_attr(img, 'src')
+  imgsrc_split <- strsplit(imgsrc, "_")
+  img_number <- as.integer(imgsrc_split[[1]][3])
+  
+  for(j in 1:47) {
+    img_numbera <- img_number - j
+    node_id <- paste0('#', img_numbera, ' .photo__intrinsic_item')
+    
+    imgsrc <- planespotter %>%
+      html_node(node_id) %>%
+      html_attr('src')
+    imgsrc
+    
+    download.file(imgsrc, destfile = basename(imgsrc))
+  }
+}
+
