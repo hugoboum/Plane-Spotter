737f72a4460bcbc85fe07b5547b43a3ab0bcbaf5
Florentin Coeurdoux <florentincoeurdoux@pc54.home>
2019-12-21 18:07:36 +0100

    -Add path to constructor and model. -Add an images folder to store the images

---
+++ CNN_model.R
@@ -0,0 +1,4 @@
+library(keras)
+install_keras()
+use_python("/Users/florentincoeurdoux/.virtualenvs/r-reticulate/bin/python")
+
--- Scrapping.R
+++ Scrapping.R
@@ -3,7 +3,7 @@
 library(rvest)
 library(dplyr)
 library(NLP)
-
+library(stringr)
 
 # First page pictures extraction ------------------------------------------
 
@@ -12,25 +12,48 @@ planespotter <- read_html("https://www.planespotters.net/photos/latest?page=1")
 img <- planespotter %>%
   html_node(xpath = '//*/img')
 
+constructor_html <- planespotter %>%
+  html_node('#1026438')
+
+constructor <- xml_attrs(xml_child(xml_child(xml_child(xml_child(constructor_html, 1), 2), 1), 4))[["title"]] %>% 
+               stringr::str_replace( "Search for Manufacturer: ", "")
+
+aircraft_type <- xml_attrs(xml_child(xml_child(xml_child(xml_child(constructor_html, 1), 2), 1), 5))[["title"]] %>% 
+                 stringr::str_replace( "Search for Aircraft Type: ", "")
+
 imgalt <- html_attr(img, 'alt')
 imgsrc <- html_attr(img, 'src')
 imgsrc_split <- strsplit(imgsrc, "_")
 img_number <- as.integer(imgsrc_split[[1]][3])
+download.file(imgsrc, destfile = paste0('Images/',aircraft_type, '-', img_number, '.jpg'))
+aicrafts <- data_frame(constructor = constructor, aircraft_type = aircraft_type)
 for(j in 1:47) {
   img_numbera <- img_number - j
   node_id <- paste0('#', img_numbera, ' .photo__intrinsic_item')
+  constructor_html <- planespotter %>%
+    html_node(paste0('#', img_numbera))
+  
+  constructor <- xml_attrs(xml_child(xml_child(xml_child(xml_child(constructor_html, 1), 2), 1), 4))[["title"]] %>% 
+    stringr::str_replace( "Search for Manufacturer: ", "")
+  
+  aircraft_type <- xml_attrs(xml_child(xml_child(xml_child(xml_child(constructor_html, 1), 2), 1), 5))[["title"]] %>% 
+    stringr::str_replace( "Search for Aircraft Type: ", "")
   
   imgsrc <- planespotter %>%
     html_node(node_id) %>%
     html_attr('src')
-  imgsrc
   
-  download.file(imgsrc, destfile = basename(imgsrc))
+  download.file(imgsrc, destfile = paste0('Images/',aircraft_type, '-', img_numbera, '.jpg'))
+  aicrafts <- rbind.data.frame(aicrafts, c(constructor, aircraft_type))
 }
+write.csv(aicrafts, file = "aicrafts.csv")
 
 # entire website images extraction ----------------------------------------
 
-for (i in 1:125){
+aicrafts <- matrix(ncol = 1, nrow = 48*2)
+nb <- 0
+for (i in 1:2){
+  nb <- nb+1
   planespotter_url <- paste("https://www.planespotters.net/photos/latest?page=",i, sep = "")
   planespotter <- read_html(planespotter_url)
   img <- planespotter %>%
@@ -40,17 +63,25 @@ for (i in 1:125){
   imgsrc <- html_attr(img, 'src')
   imgsrc_split <- strsplit(imgsrc, "_")
   img_number <- as.integer(imgsrc_split[[1]][3])
-  
+  download.file(imgsrc, destfile = paste0('Images/',imgalt, '.jpg'))
+  aicrafts[nb] <- imgalt
   for(j in 1:47) {
+    nb <- nb+1
     img_numbera <- img_number - j
     node_id <- paste0('#', img_numbera, ' .photo__intrinsic_item')
     
-    imgsrc <- planespotter %>%
-      html_node(node_id) %>%
-      html_attr('src')
-    imgsrc
-    
-    download.file(imgsrc, destfile = basename(imgsrc))
+    img <- planespotter %>%
+      html_node(node_id) 
+    imgalt <- html_attr(img, 'alt')
+    imgsrc <- html_attr(img, 'src')
+    aicrafts[nb] <- imgalt
+    if(is.na(imgsrc)){ Sys.sleep(0.5)
+    }else{
+      download.file(imgsrc, destfile = paste0('Images/',imgalt, '.jpg'))
+    }
   }
 }
+write.csv(aicrafts, file = "aicrafts.csv")
+
+
 
